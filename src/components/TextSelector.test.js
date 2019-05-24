b0a57df127a878c63751083a4055549d4b52e761
Cosmo Kent <cosmolightfoot@gmail.com>
2019-05-24 10:03:52 -0700

    TextSelector test written, passing

--- src/components/TextSelector.test.js
+++ src/components/TextSelector.test.js
@@ -4,7 +4,8 @@ import { shallow } from 'enzyme';
 
 describe('Text Selector tests', () => {
   it('renders the text selector', () => {
-    const wrapper = shallow(<TextSelector />);
+    const func = jest.fn();
+    const wrapper = shallow(<TextSelector imageUrl="www.test.com" topText="heck" bottomText="yes" handleChange={func} />);
     expect(wrapper).toMatchSnapshot();
   });
 }); 
