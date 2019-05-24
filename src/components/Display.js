22e46a6cf318d9ed7f9e25189ef802292479bdeb
Ben Parisot <me@benparisot.com>
2019-05-24 10:51:06 -0700

    refactor styles on text display

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 
 function Display(headerText) {
 
+  const textStyles = {
+    textTransform: 'uppercase',
+    fontSize: '42px',
+    fontWeight: 'bolder',
+    fontFamily: 'sans-serif'
+  };
+
 
   return (
     <section style={{ textAlign: 'center' }}>
-      <p style={{ textTransform: 'uppercase', fontSize: '42px', fontWeight: 'bolder', fontFamily: 'sans-serif' }}>{headerText.headerText}</p>
+      <p style={textStyles}>{headerText.headerText}</p>
       <img style={{ width: '300px' }} src={headerText.imageSrc} />
-      <p style={{ textTransform: 'uppercase', fontSize: '42px', fontWeight: 'bolder', fontFamily: 'sans-serif' }}>{headerText.footerText}</p>
+      <p style={textStyles}>{headerText.footerText}</p>
     </section>
   );
 }
