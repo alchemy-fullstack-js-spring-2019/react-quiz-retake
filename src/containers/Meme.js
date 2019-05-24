bf413c669aef8c96d086a0450e6c547c48605634
Sean Nelson <smnelsonn@gmail.com>
2019-05-24 10:15:53 -0700

    tweak

--- src/containers/Meme.js
+++ src/containers/Meme.js
@@ -4,9 +4,9 @@ import Display from '../components/Display';
 
 export default class Meme extends PureComponent {
   state = {
-    headerText: '',
-    imgSource: '',
-    footerText: '',
+    headerText: 'Billy',
+    imgSource: 'https://www.musicfestnews.com/wp-content/uploads/2018/10/billy-strings-festy9-festy-experience-jody-carbone-photos-2018-10-07-93.jpg',
+    footerText: 'Memes',
   }
 
   changeHandler = ({ target }) => {
