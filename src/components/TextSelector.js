d8aa907df2bfaf4edd36faeb9bbacf484756f827
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-05-24 09:39:02 -0700

    created textselector component

---
+++ src/components/TextSelector.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function TextSelector({ text, handleChange }) {
+  return (
+    <>
+      <input name="text" value={text} onChange={handleChange} />
+    </>
+  );
+}
+
+TextSelector.propTypes = {
+  text: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired
+}
+
+export default TextSelector;
