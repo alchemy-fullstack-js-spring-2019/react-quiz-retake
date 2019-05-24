db32aa2332acf6cf6b371560e52c69aa8dfd650f
Masmics <cherlocker3113@gmail.com>
2019-05-24 11:06:29 -0700

    final commit

--- src/Meme.js
+++ src/Meme.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import TextSelector from '';
 import Display from '';
 
+const image1 = 'https://images.ecosia.org/An2PbD_aWxo5_5lkuaiDNB7sl-s=/0x390/smart/http%3A%2F%2Fi0.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F001%2F178%2F872%2F3f4.jpg';
+const header1 = 'Meme Generator';
+
 export default class Meme extends Component {
   state = {
-    header: '',
-    image: '',
+    header: header1,
+    image: image1,
     footerText: ''
   }
   
