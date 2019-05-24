d20e273c9fe5076874d930b2860e8bc8b023f9ae
dreamer <wildetangeant@gmail.com>
2019-05-24 10:52:25 -0700

    ugh

--- src/components/textSelector.js
+++ src/components/textSelector.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function textSelector({ headerText, footerText, image }) {
-  return (
-     <>
-        <input onChange={headerChange} type="text" name="headerText" value={headerText} />
-        <input onChange={footerChange} type="text" name="headerText" value={footerText} />
-        <input onChange={imageChange} type="text" name="headerText" value={image} />
-        <button onClick={handleSubmit}>Submit</button>
-        
-     </>
+function textSelector({ image, headerText, footerText }) {
+  return ( 
+    <>
+    <input name="headerText" type="text" value={headerText} />
+    <input name="footerText" type="text" value={footerText} />
+    <input name="image" type="url" value={image} />
+    
+    </>
   );   
 }
 
 textSelector.propTypes = {
+  image: PropTypes.string.isRequired,
   headerText: PropTypes.string.isRequired,
   footerText: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  
-  headerChange: PropTypes.string.isRequired,
-  footerChange: PropTypes.string.isRequired,
-  imageChange: PropTypes.string.isRequired,
-
+  handleChange: PropTypes.func.isRequired
 }
 ;
 
