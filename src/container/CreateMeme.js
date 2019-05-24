7f59186c6e137f0814834268898047274f2603bb
Cara Ponzini <ponzini@gmail.com>
2019-05-24 10:51:59 -0700

    fixed display a little

--- src/container/CreateMeme.js
+++ src/container/CreateMeme.js
@@ -26,12 +26,16 @@ export default class CreateMeme extends PureComponent {
   render() {
     const { headerText, footerText, image } = this.state;
     return (
+      <>
+      <h1>make a super basic meme</h1>
+      <p>enter words, click button!</p>
       <form onSubmit={this.handleSubmit}>
         <input type="text" name="headerText" value={headerText} onChange={this.handleChange}></input>
         <input type="text" name="footerText" value={footerText} onChange={this.handleChange}></input>
         <input type="text" name="image" value={image} onChange={this.handleChange}></input>
         <button>Submit!</button>
       </form>
+      </>
     );
   }
 }
