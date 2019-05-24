8a3a5d3c58b2127149b34787b049b1aac7090a59
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-05-24 10:24:13 -0700

    bringing components together

--- src/containers/Meme.js
+++ src/containers/Meme.js
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react';
 import TextSelector from '../components/TextSelector';
+import Display from '../components/Display';
+
 
 export default class Meme extends PureComponent {
   state = {
@@ -13,13 +15,16 @@ export default class Meme extends PureComponent {
   }
 
   render() {
-    const { text } = this.state;
+    const { text, image, bottomText } = this.state;
     return (
       <>
         <TextSelector
           text={text}
+          bottomText={bottomText}
+          image={image}
           handleChange={this.handleChange}
         />
+        <Display text={text} image={image} bottomText={bottomText}/>
       </>
     );
   }
