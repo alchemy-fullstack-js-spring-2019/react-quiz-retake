bc060d4e9ad73efcc01f5929bdc6f67f5f91a5f5
Masmics <cherlocker3113@gmail.com>
2019-05-24 10:29:15 -0700

    TextSelector and its test, Display.test added

--- src/TextSelector.js
+++ src/TextSelector.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+function TextSelector({ footerText, handleChange }) {
+  return (
+    <input 
+      name="footerText"
+      type="text"
+      value={footerText}
+      onChange={handleChange}
+    />
+  );
+}
+
+TextSelector.propTypes = {
+  footerText: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired
+};
+
+export default TextSelector;
 
 // Container component
 // contains state
