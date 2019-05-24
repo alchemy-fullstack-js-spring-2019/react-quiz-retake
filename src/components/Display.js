3becf12cb8074695df578e40ee6836ca6e958111
dcornelius4 <d_cornelius94@yahoo.com>
2019-05-24 10:41:21 -0700

    made the display component

---
+++ src/components/Display.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Display({ headerText, footerText, image }) {
+  return (
+    <div>
+      <p>{headerText}</p>
+      <img name="image" value="image" src={image}></img>
+      <p>{footerText}</p>
+    </div>
+  );
+}
+
+Display.propTypes = {
+  headerText: PropTypes.string.isRequired,
+  footerText: PropTypes.string.isRequired,
+  image: PropTypes.any.isRequired
+};
+
+export default Display;
