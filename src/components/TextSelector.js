47d267fa2251e974ecceecb3bbdb21fdb5f7d9b9
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-24 09:45:20 -0700

    text selector written

---
+++ src/components/TextSelector.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function TextSelector({ headerText, footerText, image, handleChange }) {
+  return (
+    <>
+    Header: <input name="headerText" type="text" value={headerText} onChange={handleChange} />
+    Image: <input name="image" type="text" value={image} onChange={handleChange} />
+    footerText: <input name="image" type="text" value={footerText} onChange={handleChange} />
+    </> 
+  );
+}
+
+TextSelector.propTypes = {
+  headerText: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  footerText: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired
+}
