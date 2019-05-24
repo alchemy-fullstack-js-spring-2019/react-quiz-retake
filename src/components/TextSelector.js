4183803e3d1d3679cc9e6ab984e61e5544fbe0be
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-05-24 10:23:53 -0700

    fiddled with text selector

--- src/components/TextSelector.js
+++ src/components/TextSelector.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function TextSelector({ text, handleChange }) {
+function TextSelector({ text, bottomText, image, handleChange }) {
+  const imageStyle = {
+    display: 'block',
+    height: '250px',
+    width: '250px'
+  };
+  
   return (
     <>
       <input name="text" value={text} onChange={handleChange} />
+      <input style={imageStyle} name="image" type="file" value={image} onChange={handleChange} />
+      <input name="bottomText" value={bottomText} onChange={handleChange} />
     </>
   );
 }
 
 TextSelector.propTypes = {
+  image: PropTypes.string.isRequired,
+  bottomText: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
   handleChange: PropTypes.func.isRequired
 }
