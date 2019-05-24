0da5af49a0748e23c533a2d8fa09a891223e3e16
Laura Badgley <l.badgleyy@gmail.com>
2019-05-24 10:34:09 -0700

    trying to get something to render

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { headerText, footerText } from './TextSelector';
 
 export default function Display({ headerText, image, footerText }) {
   const styles = {
@@ -13,9 +14,9 @@ export default function Display({ headerText, image, footerText }) {
 
   return (
     <section style={styles}>
-      <p>{headerText}</p>
-      <img>{image}</img>
-      <p>{footerText}</p>
+      {/* <p >{headerText}</p> */}
+      <img src="http://preetkamal.com/wp-content/uploads/2019/03/Friday-Meme-Friday-Is-My-Second.jpg"></img>
+      {/* <p>{footerText}</p> */}
     </section>
   );
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,11 @@
 import React, { PureComponent } from 'react';
-import { HeaderText, FooterText } from './TextSelector';
-// import TextSelectoriu from './TextSelector';
+import { headerText, footerText } from './TextSelector';
+import Display from './Display';
 
 export default class Meme extends PureComponent {
   state = {
     headerText: '',
-    image: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiCuJHzzLTiAhXnh1QKHa3BDekQjRx6BAgBEAU&url=https%3A%2F%2Fpreetkamal.com%2F34-very-funny-friday-meme-that-definitely-make-you-smile%2F&psig=AOvVaw2g6JAuHhQLAZoPQR0AcXCa&ust=1558802213391446',
+    image: 'http://preetkamal.com/wp-content/uploads/2019/03/Friday-Meme-Friday-Is-My-Second.jpg',
     footerText: ''
   }
 
@@ -18,9 +18,9 @@ export default class Meme extends PureComponent {
 
     return (
       <div>
-        <HeaderText text={headerText} handleChange={this.handleChange}/>
+        <Display text={headerText} handleChange={this.handleChange}/>
         <img alt="Friday meme">{image}</img>
-        <FooterText text={footerText} handleChange={this.handleChange}/>
+        <Display text={footerText} handleChange={this.handleChange}/>
       </div>
     );
   }
