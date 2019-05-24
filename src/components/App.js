5631df6b8a3b053f967a6f887206f708f6420a90
Mallarie <mallariemilano@gmail.com>
2019-05-24 10:17:40 -0700

    app component and test

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './header/header';
+import Footer from './footer/footer';
+
+export default function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
